72771d61532cae5be01470e3100cbe713bb0d77b
newsspy <158444288+newsspy@users.noreply.github.com>
2024-03-19 20:28:14 +0530

    Add files via upload

---
+++ src/app/data.js
@@ -0,0 +1,81 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Project 1",
+    description: "soon",
+    // date: "2022-08-15",
+    // demoLink: "https://.example.com",
+  },
+  {
+    id: 2,
+    name: "Project 2",
+    description: "soon",
+    // date: "2022-06-20",
+    // demoLink: "https://.example.com",
+  },
+  {
+    id: 3,
+    name: "Project 3",
+    description: "soon",
+    // date: "2022-09-10",
+    // demoLink: "https://.example.com",
+  },
+  {
+    id: 4,
+    name: "Project 4",
+    description: "soon",
+    // date: "2022-05-30",
+    // demoLink: "https://example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/tanujjoliya1",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://www.instagram.com/tanujjoliya",
+    icon: "instagram",
+    newTab: true,
+  },
+  {
+    label: "agency",
+    link: "https://www.instagram.com/socialorixx",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
